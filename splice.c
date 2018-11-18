99ea53ce899e06b51ef3eeced6253fcf4aa75387
danishprakash <grafitykoncept@gmail.com>
2018-11-18 15:18:49 +0530

    working splice.c

--- splice.c
+++ splice.c
@@ -1,4 +1,5 @@
-#define GNU_SOURCE
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -7,48 +8,48 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-// create pipe
-static int fd_pipe[2];
-// TODO: error handling for creation of pipe
-//
-pipe(fd_pipe);
-
-off_t fsize(const char *filename) {
-    struct stat st; 
-
-    if (stat(filename, &st) == 0)
-        return st.st_size;
-
-    return -1; 
-}
-
-int splice_copy(int fd_out, int fd_in, ssize_t len) {
-    ssize_t bytes, bytes_sent, bytes_in_pipe;
-    size_t total_bytes_sent;
+ssize_t splice_copy(int fd_out, int fd_in, size_t len) {
+    int fd_pipe[2];
+    ssize_t total_bytes_sent = 0;
+    size_t buf_size = 128;
     loff_t in_off = 0;
     loff_t out_off = 0;
 
-    while(total_bytes_sent < len) {
+    if (pipe(fd_pipe) < 0) {
+        perror("Error creating pipe");
+        return 1;
+    }
+
+    printf("len before: %zd\n", len);
+    printf("stat before: %zd\n", buf_size);
+
+    while(len > 0) {
+        if (buf_size > len) buf_size = len;
         // splice data to pipe
-        if ((bytes = splice(fd_in, in_off, fd_pipe[1], NULL, len-total_bytes_sent)) <= 0) {
-            perror("splice");
+        if ((splice(fd_in, &in_off, fd_pipe[1], NULL, buf_size, SPLICE_F_MOVE)) == -1) {
+            perror("assplice");
             return 1;
         }
 
         // splice data from pipe to fd_out
-        if ((bytes = splice(fd_pipe[0], NULL, fd_out, out_off, bytes)) <= 0) {
+        if ((splice(fd_pipe[0], NULL, fd_out, &out_off, buf_size, SPLICE_F_MOVE)) == -1) {
             perror("splice");
             return 1;
         }
 
-        total_bytes_sent += bytes;
+        len -= buf_size;
+        printf("total bytes: %zd\n", len);
     }
-    return total_bytes_sent;
+    return 1;
 }
 
 int main(){
-    FILE *fin, *fout;
-    fin = fopen("read.txt", "r");
-    fout = fopen("write.txt", "w");
-    printf(splice_copy(fileno(fin), fileno(fout), fsize(fin)));
+    int fin, fout;
+    size_t fsize;
+    fin = open("read.txt", O_RDONLY);
+    fout = open("write.txt", O_WRONLY);
+    fsize = lseek(fin, 0, SEEK_END);
+
+    /* printf("%zd", fsize); */
+    printf("%zd", splice_copy(fout, fin, fsize));
 }
