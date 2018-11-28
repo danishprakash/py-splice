407944ba4ab461df4aa5d85d451b5990225b2035
danishprakash <grafitykoncept@gmail.com>
2018-11-28 14:39:46 +0530

    conform to GNU C guidelines*

--- splicemodule.c
+++ splicemodule.c
@@ -11,28 +11,36 @@
 // TODO: comments and explanation
 // TODO: conditionals for different distros
 // TODO: function declarations for all
+// TODO: sane perror args
+// TODO: structure error handling part
 
-// declarations
-int is_fd_valid(int);
+// function declarations
 size_t fsize(int);
+int is_fd_valid(int);
+PyMODINIT_FUNC PyInit_splice();
 size_t splice_copy(int, int, int, size_t);
 static PyObject *method_splice(PyObject *, PyObject *);
-PyMODINIT_FUNC PyInit_splice();
 
-// return size of file using file descriptor number
-size_t fsize(fd) {
+// return size of file using file descriptor 
+size_t 
+fsize(fd)
+{
     size_t fsize;
     fsize = lseek(fd, 0, SEEK_END);
     return fsize;
 }
 
 // check if file descriptor is valid
-int is_fd_valid(fd) {
+int 
+is_fd_valid(fd)
+{
   return fcntl(fd, F_GETFD);
 }
 
-// move data from fd_in tp fd_out using splice(2)
-size_t splice_copy(int fd_in, int fd_out, int offset, size_t len) {
+// splice(2) syscall
+size_t 
+splice_copy(int fd_in, int fd_out, int offset, size_t len)
+{
     int fd_pipe[2];
     size_t buf_size = 4096;
     size_t bytes = 0;
@@ -41,24 +49,28 @@ size_t splice_copy(int fd_in, int fd_out, int offset, size_t len) {
     off_t out_off = 0;
 
 
-    if (pipe(fd_pipe) < 0) {
+    if (pipe(fd_pipe) < 0)
+    {
         perror("Error creating pipe");
         return 1;
     }
 
-    while(len > 0) {
+    while(len > 0)
+    {
       if (buf_size > len) buf_size = len;
       
       // splice data to pipe
-      if ((bytes = splice(fd_in, &in_off, fd_pipe[1], NULL, buf_size, SPLICE_F_MOVE)) == -1) {
-        perror("splice");
+      if ((bytes = splice(fd_in, &in_off, fd_pipe[1], NULL, buf_size, SPLICE_F_MOVE)) == -1)
+      {
+        perror("Error moving data from `fd_in`");
         return -1;
       }
 
       // splice data from pipe to fd_out
-      if ((bytes = splice(fd_pipe[0], NULL, fd_out, &out_off, buf_size, SPLICE_F_MOVE)) == -1) {
-          perror("splice");
-          return -1;
+      if ((bytes = splice(fd_pipe[0], NULL, fd_out, &out_off, buf_size, SPLICE_F_MOVE)) == -1)
+      {
+        perror("Error moving data to `fd_out`");
+        return -1;
       }
 
       len -= buf_size;
@@ -67,33 +79,40 @@ size_t splice_copy(int fd_in, int fd_out, int offset, size_t len) {
     return total_bytes_sent;
 }
 
-// TODO: check for splice(2) availability
 static PyObject *
-method_splice(PyObject *self, PyObject *args){
+method_splice(PyObject *self, PyObject *args)
+{
     int fd_in, fd_out, offset, nbytes;
     int status = -1;
     size_t len;
 
     if (!PyArg_ParseTuple(args, "iiii", &fd_in, &fd_out, &offset, &nbytes))
+    {
             return NULL;
+    }
 
-    if (is_fd_valid(fd_in, fd_out) == -1 || is_fd_valid(fd_out) == -1) {
+    if (is_fd_valid(fd_in) == -1 || is_fd_valid(fd_out) == -1)
+    {
       PyErr_SetString(PyExc_ValueError, "Invalid file descriptor");
       return NULL;
     }
 
-    if (nbytes) {
-      if (nbytes > fsize(fd_in)){
+    if (nbytes)
+    {
+      if (nbytes > fsize(fd_in))
+      {
         PyErr_SetString(PyExc_OverflowError, "Length overflow error");
         return NULL;
       }
       len = nbytes;
-    } else {
+    }
+    else
+    {
       len = (size_t)fsize(fd_in);
     }
     
-    // TODO: all of the error checking and raising exceptions would be done here
-    if (offset > len) {
+    if (offset > len)
+    {
       PyErr_SetString(PyExc_OverflowError, "Offset overflow error");
       return NULL;
     }
@@ -117,7 +136,8 @@ static struct PyModuleDef splicemodule = {
 };
 
 PyMODINIT_FUNC
-PyInit_splice(void) {
+PyInit_splice(void)
+{
     return PyModule_Create(&splicemodule);
 }
 
@@ -125,7 +145,8 @@ int main(int argc, char *argv[])
 {
     wchar_t *program = Py_DecodeLocale(argv[0], NULL);
 
-    if (program == NULL) {
+    if (program == NULL)
+    {
         fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
         exit(1);
     }
