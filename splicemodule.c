9d34cce6ab6cbfd31af685982d99f8f5e1e4c67c
danishprakash <grafitykoncept@gmail.com>
2018-11-30 13:38:24 +0530

    splicemodule.c: add comment explanations

--- splicemodule.c
+++ splicemodule.c
@@ -1,26 +1,23 @@
 #define _GNU_SOURCE
 
-#include <Python.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <Python.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <unistd.h>
-
-/* TODO: comments and explanation */
-/* TODO: conditionals for different distros */
-/* TODO: pass flags to splice(2) calls */
 
 /* function declarations */
 size_t fsize(int);
 int is_fd_valid(int);
 PyMODINIT_FUNC PyInit_splice();
-size_t splice_copy(int, int, int, size_t);
 int validate_arguments(int, int, int, int *);
+size_t splice_copy(int, int, int, size_t, int);
 static PyObject *method_splice(PyObject *, PyObject *, PyObject *);
 
-/* return size of file using file descriptor  */
+/* return size of file 
+ * using file descriptor */
 size_t 
 fsize(fd)
 {
@@ -29,7 +26,8 @@ fsize(fd)
     return fsize;
 }
 
-/* check if file descriptor is valid */
+/* check if file 
+ * descriptor is valid */
 int 
 is_fd_valid(fd)
 {
@@ -38,15 +36,14 @@ is_fd_valid(fd)
 
 /* splice(2) syscall */
 size_t 
-splice_copy(int fd_in, int fd_out, int offset, size_t len)
+splice_copy(int fd_in, int fd_out, int offset, size_t len, int flags)
 {
     int fd_pipe[2];
-    int flags = 0;
-    size_t buf_size = 4096;
-    size_t bytes = 0;
-    size_t total_bytes_sent = 0;
-    off_t in_off = (off_t)offset;
     off_t out_off = 0;
+    off_t in_off = (off_t)offset;
+    size_t bytes = 0;
+    size_t buf_size = 4096;
+    size_t total_bytes_sent = 0;
 
     if (pipe(fd_pipe) < 0)
     {
@@ -81,7 +78,6 @@ splice_copy(int fd_in, int fd_out, int offset, size_t len)
 }
 
 /* validate arguments */
-/* static PyObject * */
 int
 validate_arguments(int in, int out, int offset, int *nbytes)
 {
@@ -93,6 +89,7 @@ validate_arguments(int in, int out, int offset, int *nbytes)
 
     if (*nbytes)
     {
+        /* check for length overflow */
         if (*nbytes > fsize(in))
         {
             PyErr_SetString(PyExc_OverflowError, "Length overflow error");
@@ -104,6 +101,7 @@ validate_arguments(int in, int out, int offset, int *nbytes)
         *nbytes = (size_t)fsize(in);
     }
 
+    /* check for offset overflow */
     if (offset > *nbytes)
     {
         PyErr_SetString(PyExc_OverflowError, "Offset overflow error");
@@ -115,7 +113,7 @@ validate_arguments(int in, int out, int offset, int *nbytes)
 static PyObject *
 method_splice(PyObject *self, PyObject *args, PyObject *kwdict)
 {
-    int flag = 0;
+    int validated = 0;
     int out = -1, in = -1;
     int status = -1, offset = 0, flags = 0, nbytes = 0;
     static char *keywords[] = {"in", "out", "offset", "nbytes", "flags", NULL};
@@ -126,41 +124,51 @@ method_splice(PyObject *self, PyObject *args, PyObject *kwdict)
         return NULL;
     }
 
-    flag = validate_arguments(in, out, offset, &nbytes);
-    if (flag == -1) 
+    validated = validate_arguments(in, out, offset, &nbytes);
+    if (validated == -1) 
     {
         return NULL;
     }
 
-    status = splice_copy(in, out, offset, (size_t)nbytes);
+    status = splice_copy(in, out, offset, (size_t)nbytes, flags);
 
     return PyLong_FromLong(status);
 }
 
+/* structure used to describe method 
+ * accessible in Python */ 
 static PyMethodDef SpliceMethods[] = {
-    {"splice",  method_splice, METH_VARARGS | METH_KEYWORDS, "Python interace for splice(2) system call."},
+    {
+        "splice", /* name of method accessible in Python */
+        method_splice, /* pointer to the actual function */
+        METH_VARARGS | METH_KEYWORDS, /* flag bits indicating calling convention */
+        "Python interace for splice(2) system call." /* docstring content */
+    },
     {NULL, NULL, 0, NULL}
 };
 
+/* structure that holds information needed
+ * to create a module object */ 
 static struct PyModuleDef splicemodule = {
     PyModuleDef_HEAD_INIT,
-    "splice",
-    NULL, 
-    -1,  
-    SpliceMethods
+    "splice", /* name of module */
+    NULL, /* docstring for module */
+    -1, /* no support for sub-interpretors */
+    SpliceMethods /* pointer to table of module-level functions */
 };
 
+/* specifies PyObject * as return type,
+ * declares any special linkage declarations
+ * required by the platform */
 PyMODINIT_FUNC
 PyInit_splice(void)
 {
     PyObject *module;
     module = PyModule_Create(&splicemodule);
 
+/* define constants for splice(2) flags */
 #ifdef SPLICE_F_MOVE
     PyModule_AddIntConstant(module, "SPLICE_F_MOVE", SPLICE_F_MOVE);
-#endif
-#ifdef SPLICE_F_NONBLOCK
-    PyModule_AddIntConstant(module, "SPLICE_F_NONBLOCK", SPLICE_F_NONBLOCK);
 #endif
 #ifdef SPLICE_F_MORE
     PyModule_AddIntConstant(module, "SPLICE_F_MORE", SPLICE_F_MORE);
@@ -168,6 +176,9 @@ PyInit_splice(void)
 #ifdef SPLICE_F_GIFT
     PyModule_AddIntConstant(module, "SPLICE_F_GIFT", SPLICE_F_GIFT);
 #endif    
+#ifdef SPLICE_F_NONBLOCK
+    PyModule_AddIntConstant(module, "SPLICE_F_NONBLOCK", SPLICE_F_NONBLOCK);
+#endif
 
     return module;
 }
